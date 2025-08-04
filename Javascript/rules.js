2e830da1e7e19a1ae7fbfcafb27e016dade0cd5f
abhishek <abhishekprasad7250@gmail.com>
2025-08-04 22:27:56 +0530

    add this

---
+++ Javascript/rules.js
@@ -0,0 +1,5 @@
+let username = "abhishek" ;;
+let $price = 100;
+let _year = 2022;
+let isActive = true;
+console.log(username, $price, _year, isActive); // Output: abhishek 100 2022 true"
--- Javascript/variables.js
+++ Javascript/variables.js
@@ -5,7 +5,10 @@ const c = 30;
 var d ="Hello, World!"; // old style variable
 var e = "1000"; // old style variable
 
-console.log(a, b, c, d, e); // Output: 10 20 30
+let f ="abhishek";
+let g = "2002"
+
+console.log(a, b, c, d, e, f, g); // Output: 10 20 30
 
 // new style variables
 console.log("Hello, World!"); // Output: Hello, World!
