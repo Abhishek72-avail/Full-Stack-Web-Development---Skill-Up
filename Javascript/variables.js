feec6a8bcb7a769590ca75390d0d3b5fdf5fa54e
abhishek <abhishekprasad7250@gmail.com>
2025-08-04 22:20:20 +0530

    add code

---
+++ Javascript/variables.js
@@ -0,0 +1,11 @@
+var  a= 10 //old style
+let b =20;
+const c = 30;
+
+var d ="Hello, World!"; // old style variable
+var e = "1000"; // old style variable
+
+console.log(a, b, c, d, e); // Output: 10 20 30
+
+// new style variables
+console.log("Hello, World!"); // Output: Hello, World!
